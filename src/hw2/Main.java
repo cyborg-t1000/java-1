685e268cc0ed683634f9126d3c1b7fa5ccdde900
Maxim Nekrasov <kgtu@mail.ru>
2020-11-20 09:43:25 -0500

    HW 2, task 1

---
+++ src/hw2/Main.java
@@ -0,0 +1,19 @@
+package hw2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] == 0 ? 1 : 0;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
