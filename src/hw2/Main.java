1ca90f0b1f7c70eb26ba53019e736bb1e3b06ffa
Maxim Nekrasov <kgtu@mail.ru>
2020-11-20 09:49:30 -0500

    HW 2, task 2

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -9,11 +9,18 @@ public class Main {
         // С помощью цикла и условия заменить 0 на 1, 1 на 0;
 
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] == 0 ? 1 : 0;
         }
         System.out.println(Arrays.toString(arr));
+
+        // 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+
+        int[] arr2 = new int[8];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = i * 3;
+        }
+        System.out.println(Arrays.toString(arr2));
 
     }
 }
