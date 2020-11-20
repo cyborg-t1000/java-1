2b7c173bd6eacf87ca04ae892b67cc9f3ee2eb2e
Maxim Nekrasov <kgtu@mail.ru>
2020-11-20 11:33:45 -0500

    HW 2, task 3

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -22,5 +22,15 @@ public class Main {
         }
         System.out.println(Arrays.toString(arr2));
 
+        // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+
+        int[] arr3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr3.length; i++) {
+            {
+                if (arr3[i] < 6) arr3[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr3));
+
     }
 }
