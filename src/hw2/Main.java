c43c2ef730f1a34d2501cade3f2f05f6e26b0eb1
Maxim Nekrasov <kgtu@mail.ru>
2020-11-20 11:50:33 -0500

    HW 2, task 5

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -44,5 +44,15 @@ public class Main {
             System.out.println(Arrays.toString(ints));
         }
 
+        // 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+
+        int[] arr5 = new int[]{1, 5, 3, 2, 11, -4, 5, 2, 4, 8, 9, 1};
+        int min = arr5[0];
+        int max = arr5[0];
+        for (int i = 1; i < arr5.length; i++) {
+            if (arr5[i] < min) min = arr5[i];
+            if (arr5[i] > max) max = arr5[i];
+        }
+        System.out.printf("Min: %d, Max: %d", min, max);
     }
 }
